ff8f21225ab995c96922becc6c5c6b597858e2d0
br1anmoraes <brianmoraes2001@gmail.com>
2023-05-15 18:10:15 -0300

    Repositorio - Arias Web Designers

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username === "arias" && password === "proyectoits") {
+        window.location.href = '/html/tareas.html';
+        var isLoggedIn = true;
+    } else {
+        document.getElementById('message').textContent = 'Nombre de usuario o contraseña incorrectos.';
+    }
+
+    event.preventDefault();
+}
+
