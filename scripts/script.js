c2bee6e08503d5ef108b0fd2eb6a41d0fe463351
br1anmoraes <133707082+br1anmoraes@users.noreply.github.com>
2023-05-16 00:32:12 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@ function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if (username === "Arias" || username === "arias"  && password === "proyectoits") {
+    var username = username.toLowerCase();
+    if (username === "arias" && password === "proyectoits") {
         window.location.href = '/html/tareas.html';
     } else {
         document.getElementById('message').textContent = 'Nombre de usuario o contraseña incorrectos.';
@@ -10,4 +11,6 @@ function login() {
 
     event.preventDefault();
 }
+
+
 
